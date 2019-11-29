6806b07296b93998401de5e0521d8b14f64cc2db
Jere Käpyaho <jere@coniferproductions.com>
2019-11-29 09:01:23 +0200

    Moved byte extensions to their own source file

---
+++ Common/ByteExtensions.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KSynthLib.Common
+{
+    // Byte extensions from https://derekwill.com/2015/03/05/bit-processing-in-c/
+    public static class ByteExtensions
+    {
+        public static bool IsBitSet(this byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+ 
+            return (b & (1 << pos)) != 0;
+        }
+ 
+        public static byte SetBit(this byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+ 
+            return (byte)(b | (1 << pos));
+        }
+ 
+        public static byte UnsetBit(this byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+ 
+            return (byte)(b & ~(1 << pos));
+        }
+ 
+        public static byte ToggleBit(this byte b, int pos)
+        {
+            if (pos < 0 || pos > 7)
+                throw new ArgumentOutOfRangeException("pos", "Index must be in the range of 0-7.");
+ 
+            return (byte)(b ^ (1 << pos));
+        }
+ 
+        public static string ToBinaryString(this byte b)
+        {
+            return Convert.ToString(b, 2).PadLeft(8, '0');
+        }
+
+        public static sbyte ToSignedByte(this byte b)
+        {
+            return unchecked((sbyte)b);
+        }
+    }
+
+    public static class SignedByteExtensions
+    {
+        public static byte ToByte(this sbyte s)
+        {
+            return unchecked((byte)s);
+        }
+    }
+}
